a60a8aafc0c44509f407535ad2f01ee2efc1e42b
yabeye <88554326+yabeye@users.noreply.github.com>
2022-03-03 12:48:18 +0300

    Updated Message.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     import generator
     g = generator.Generator()
     g.generate_all_combs([])
-    print('All android patterns are generated, Check out "all_patterns.txt" file ')
+    print('All possible android patterns are generated, Check out "all_patterns.txt" file in main.py file\'s current directory.')
 
 
 if __name__ == "__main__":
